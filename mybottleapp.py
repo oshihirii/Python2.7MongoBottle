0731557abb4dea55456af334fb4e0f2a5732607d
oshihirii <oshihirii@gmail.com>
2013-09-21 16:25:06 +1000

    updating mybottleapp.py

--- mybottleapp.py
+++ mybottleapp.py
@@ -4,15 +4,20 @@ import pymongo
 @route('/')
 def index():
     try:
-        dbhost = 'admin:password@127.x.xx.xxx:27017/'
+        dbhost = 'admin:username@127.x.xx.xxx:27017/'
         dbname = 'mydbname'
         connection = pymongo.MongoClient("mongodb://" + dbhost)
         db = connection.dbname
     except:
          return ("Couldn't connect to database")
     collection = db.myCollection
-    result = collection.find_one()
-    return template('page1',{'dbresult':result['content']})
+    query = {'blah':1}
+    projection = {'content': 1, '_id': 0}
+    cursor = collection.find_one(query,projection)
+    try:
+        return template('page1',{'dbresult':cursor})
+    except:
+        return ("Problem passing data to template")
 
 import os
 from bottle import TEMPLATE_PATH
