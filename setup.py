3b04f0461bb3d63466e9851d01c813e2aefaebc0
oshihirii <oshihirii@gmail.com>
2013-09-21 15:18:52 +1000

    updating

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup
 
 setup(name='YourAppName', version='1.0',
-      description='Enter a description here',
+      description='OpenShift Python-2.7 Application',
       author='Your Name', author_email='youremail@example.org',
       url='http://www.your-url.com',
 
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
-      install_requires=['WebOb',
-                          'bottle',
-                        #  'mysql-connector-python',
-                          'pymongo',
-                        #  'psycopg2',
-      ],
+      install_requires=['bottle','pymongo'],
      )
